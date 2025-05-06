e5402877c05c151b75dac926c6ef569eb65ce96e
0x4272616E646F6E <59065614+0x4272616E646F6E@users.noreply.github.com>
2025-05-05 23:21:02 -0400

    ran gofmt -s
    Signed-off-by: 0x4272616E646F6E <59065614+0x4272616E646F6E@users.noreply.github.com>

--- internal/shared/queue.go
+++ internal/shared/queue.go
@@ -3,34 +3,34 @@ package shared
 import "sync"
 
 type MessageQueue[T any] struct {
-        mu    sync.Mutex
-        queue []T
+	mu    sync.Mutex
+	queue []T
 }
 
 func NewMessageQueue[T any]() *MessageQueue[T] {
-        return &MessageQueue[T]{queue: make([]T, 0)}
+	return &MessageQueue[T]{queue: make([]T, 0)}
 }
 
 func (q *MessageQueue[T]) Enqueue(msg T) {
-        q.mu.Lock()
-        defer q.mu.Unlock()
-        q.queue = append(q.queue, msg)
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	q.queue = append(q.queue, msg)
 }
 
 func (q *MessageQueue[T]) Dequeue() (T, bool) {
-        q.mu.Lock()
-        defer q.mu.Unlock()
-        if len(q.queue) == 0 {
-                var zero T
-                return zero, false
-        }
-        msg := q.queue[0]
-        q.queue = q.queue[1:]
-        return msg, true
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	if len(q.queue) == 0 {
+		var zero T
+		return zero, false
+	}
+	msg := q.queue[0]
+	q.queue = q.queue[1:]
+	return msg, true
 }
 
 func (q *MessageQueue[T]) Len() int {
-        q.mu.Lock()
-        defer q.mu.Unlock()
-        return len(q.queue)
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	return len(q.queue)
 }
